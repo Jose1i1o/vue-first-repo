11ec20b68ad5260b40961d536e290eecebabf08a
Jose1i1o <guadix88@gmail.com>
2022-04-26 00:00:19 +0200

    feat: structuring our data, import data from JSON files in a Vue App and display a list of elements in Vue 3

---
+++ src/components/PageHome.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>Hello from the first page</div>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{ thread.title }}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>{{userById(postById(postId).userId).name }}</p>
+      <p>{{postById(postId).text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+export default {
+  data () {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  },
+  methods: {
+    postById (postId) {
+      return this.posts.find(post => post.id === postId)
+    },
+    userById (userId) {
+      return this.users.find(post => post.id === userId)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
